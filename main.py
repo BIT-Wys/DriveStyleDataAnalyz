b260f3b03079a69d29fee25cfc0415a6b37c5e06
wys <1119551805@qq.com>
2024-07-04 21:33:30 +0800

    Init repository
    Signed-off-by: wys <1119551805@qq.com>

---
+++ main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import os
+from read_table import ReadTable
+# 加载数据
+data = ReadTable()
+cluster_efficiency_map = {}
+vehicle_ids = data['Vehicle_ID'].unique()
+
+for vehicle_id in vehicle_ids:
+    vehicle_data = data[data['Vehicle_ID'] == vehicle_id]
+    time_headway = vehicle_data['Time_headway']
+    space_headway = vehicle_data['Space_headway']
+    lane_id = vehicle_data['Lane_ID']
+    velocity = np.hypot(vehicle_data['x_Velocity'], vehicle_data['y_Velocity'])
+
+    for j in range(1, len(lane_id)):
+        if lane_id.iloc[j] != lane_id.iloc[j-1] and 1 < time_headway.iloc[j] < 500:
+            cluster = vehicle_data['Cluster'].iloc[j] + 1  # 获取该车辆的簇ID
+            ego_vel = velocity.iloc[j]
+            target_vel = ego_vel + space_headway.iloc[j] / time_headway.iloc[j]
+            efficiency = (target_vel - ego_vel) / ego_vel
+
+            if efficiency < 0.1 or np.isnan(efficiency):
+                continue
+
+            if cluster in cluster_efficiency_map:
+                cluster_efficiency_map[cluster].append(efficiency)
+            else:
+                cluster_efficiency_map[cluster] = [efficiency]
+
+keys = list(cluster_efficiency_map.keys())
+values = [cluster_efficiency_map[key] for key in keys]
+num_cols = max(len(v) for v in values)
+map_matrix = np.zeros((len(keys), num_cols))
+
+
+output_file_name = 'lane_change_efficiency.csv'
+np.savetxt(output_file_name, map_matrix, delimiter=',')
+
+print("Excel file has been created successfully!")
---
+++ read_table.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+def ReadTable():
+    # 定义包含 CSV 文件的文件夹路径
+    folder_path = './rl1'  # 替换为你的文件夹路径
+    # 获取文件夹中所有 CSV 文件的信息
+    file_list = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+
+    # 初始化一个空列表以存储数据框架
+    data_frames = []
+
+    # 循环读取每个文件，并将其数据框架追加到列表
+    for file_name in file_list[:500]:  # 限制为前500个文件，如果文件夹中文件少于500，将读取所有文件
+        current_file_path = os.path.join(folder_path, file_name)
+        current_data_frame = pd.read_csv(current_file_path)
+
+        # 检查 'Following_Lane_ID' 是否存在并为字符串列（等效于 MATLAB 中的 cell array）
+        if pd.api.types.is_string_dtype(current_data_frame['Following_Lane_ID']):
+            data_frames.append(current_data_frame)
+
+    # 合并所有数据框架为一个单一的数据框架
+    combined_data = pd.concat(data_frames, ignore_index=True)
+    return combined_data
+
+# # 调用函数并将结果赋值给变量
+# data = read_table()
